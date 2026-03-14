ed96c12c649dd137b431d1e87aa1b06e4be29d73
timonteutelink <timon@teutelink.nl>
2026-03-14 17:55:29 +0100

    very nice

--- nix/lib/home-option-default-nixos.nix
+++ nix/lib/home-option-default-nixos.nix
@@ -5,6 +5,7 @@ let
     , inputs ? { }
     , mirror ? null
     , type ? null
+    , fallback ? null
     }: (
       let
         osConfig =
@@ -12,10 +13,29 @@ let
           then inputs.osConfig else null;
 
         optionDefault = (import ./get-nested-attr.nix { inherit lib; }).get-nested-attr (lib.splitString "." mirror) osConfig;
+        fallbackDefault =
+          if type == null then
+            null
+          else if type.check false then
+            false
+          else if type.check "" then
+            ""
+          else if type.check [ ] then
+            [ ]
+          else if type.check { } then
+            { }
+          else
+            null;
       in
       lib.mkOption {
         inherit description type;
-        default = optionDefault;
+        default =
+          if optionDefault != null then
+            optionDefault
+          else if fallback != null then
+            fallback
+          else
+            fallbackDefault;
       }
     );
 in
